e11cc9a751447cf97bd7708e70c87b1e39043e83
Michael Cook Ryan <michaelcookryan@users.noreply.github.com>
2018-02-18 14:52:42 -0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {  
     // Database connection information
-    'database': 'mongodb://mike:mike@ds127436.mlab.com:27436/zenith_events',
+    'database': 'mongodb://YOUR-MONGODB-PATH-GOES-HERE/zenith_events',
 
     // Setting port for server
     'port': process.env.PORT || 3000  
